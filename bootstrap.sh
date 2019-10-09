58c949d7c3a1ff778ef6de89d1b585abab8a9321
Rylee Lyman <rylee.lyman@tufts.edu>
2019-10-09 19:28:10 +0000

    draft bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+apt-get install fuse libfuse2 git python3-pip ack-grep -y
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
+chmod +x nvim
+mv nvim /usr/bin
+ln -srn dotfiles/.config .config
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip3 install --user pynvim
+wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh
